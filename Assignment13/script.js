a8084a1eb0ad990b3b0ac281c10af6bd41a40b27
Nahid Ebrahimian <82975802+NahidEbrahimian@users.noreply.github.com>
2021-12-20 23:18:57 +0330

    init

---
+++ Assignment13/script.js
@@ -0,0 +1,112 @@
+var cells = document.getElementsByClassName("cell");
+var score_x = document.getElementById("score-x");
+var score_o = document.getElementById("score-o");
+
+var score_x_counter = 0;
+var score_o_counter = 0;
+
+var o_winner = 0;
+var x_winner = 0;
+
+var winner = document.getElementById("winner");
+var winner_background = document.getElementById("display-winner");
+
+// ui - front-end
+var buttons = [[cells[0], cells[1], cells[2]],
+                [cells[3], cells[4], cells[5]],
+                [cells[6], cells[7], cells[8]]];
+
+// logic - back-end
+var flags = [[null, null, null],
+            [null, null, null],
+            [null, null, null]];
+
+var ply = "X";
+
+function show_index(x, y){
+    if(flags[x][y] == null){
+    // alert(x + " " + y);
+        if(ply == "X"){
+            flags[x][y] = "X";
+        buttons[x][y].innerHTML = "X";
+        buttons[x][y].classList.add("X");
+        ply = "O";
+        }
+
+        else if(ply == "O"){
+            flags[x][y] = "O"
+            buttons[x][y].innerHTML = "O";
+            buttons[x][y].classList.add("O");
+            ply = "X";
+            }
+    }
+    // else{
+    //     alert("please try agane!!!")
+    // }
+    check_game()
+    display_winner()
+}
+
+function check_game(){
+    for(let  i = 0; i < 3; i++){
+        if(flags[i][0] == "X" && flags[i][1] == "X" && flags[i][2] == "X"){
+            score_x_counter++;
+            score_x.innerHTML = score_x_counter;
+            x_winner = 1;
+
+        }
+        if(flags[i][0] == "O" && flags[i][1] == "O" && flags[i][2] == "O"){
+            score_o_counter++;
+            score_o.innerHTML = score_o_counter;
+            o_winner = 1;
+        }
+        if(flags[0][i] == "X" && flags[1][i] == "X" && flags[2][i] == "X"){
+            score_x_counter++;
+            score_x.innerHTML = score_x_counter;
+            x_winner = 1;
+
+        }
+        if(flags[0][i] == "O" && flags[1][i] == "O" && flags[2][i] == "O"){
+            score_o_counter++;
+            score_o.innerHTML = score_o_counter;
+            o_winner = 1;
+
+        }
+    }
+    if(flags[0][0] == "X" && flags[1][1] == "X" && flags[2][2] == "X"){
+        score_x_counter++;
+        score_x.innerHTML = score_x_counter;
+        x_winner = 1;
+
+    }
+    if(flags[0][0] == "O" && flags[1][1] == "O" && flags[2][2] == "O"){
+        score_o_counter++;
+        score_o.innerHTML = score_o_counter;
+        o_winner = 1;
+
+    }
+    if(flags[2][0] == "O" && flags[1][1] == "O" && flags[0][2] == "O"){
+        score_o_counter++;
+        score_o.innerHTML = score_o_counter;
+        o_winner = 1;
+
+    }
+    if(flags[2][0] == "X" && flags[1][1] == "X" && flags[0][2] == "X"){
+        score_x_counter++;
+        score_x.innerHTML = score_x_counter;
+        x_winner = 1;
+
+    }
+}
+
+function display_winner(){
+        if(x_winner == 1){
+            winner.innerHTML = "X Winner";
+            // winner_background.classList.add("winner")
+        }
+        else if(o_winner == 1){
+            winner.innerHTML = "O Winner";
+        }
+        
+}
+
