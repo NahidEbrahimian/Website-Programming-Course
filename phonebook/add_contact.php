abf70a6e2fb10db63a8942b6fd43247b7b5e34a8
Nahid Ebrahimian <82975802+NahidEbrahimian@users.noreply.github.com>
2022-02-26 08:07:37 +0330

    edit tags

--- phonebook/add_contact.php
+++ phonebook/add_contact.php
@@ -13,38 +13,28 @@
             <div class="mt-1">
                 <form  method="post" action="add_new_contact.php">
                     <div class="rounded-2 bg-dark px-4 pb-4">
-
                         <div class="row text-center">
-                            <h3 class="mt-3 text-white" >Add New Contact</h2>
+                            <h3 class="mt-3 text-white" >Add New Contact</h3>
                         </div>
-
                         <hr style="color: white" class="px-0">
-
                         <label for="inputEmail4" class="form-label text-white mt-3"  >First Name</label>
                         <input type="text" class="form-control m-0" name="name" >
-
                         <label for="inputEmail4" class="form-label text-white mt-3" >Last Name</label>
                         <input type="text" class="form-control" name="family">
-
                         <label for="inputPassword4" class="form-label text-white mt-3" >Mobile Number</label>
                         <input type="text" class="form-control" name="mobile">
-
                         <label for="inputPassword4" class="form-label text-white mt-3" >Phone Number</label>
                         <input type="text" class="form-control" name="phone">
-
                         <label for="inputEmail4" class="form-label text-white mt-3" >Email</label>
                         <input type="email" class="form-control" name="email">
-
                     </div>
-
                     <div>
                         <button type="submit" class="btn btn-primary text-white mt-2">Save Contact</button>
                     </div>
-
                 </form>
             </div>
-
         </div>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
         <script src="js/bootstrap.js"></script>
     </body>
+</html>
