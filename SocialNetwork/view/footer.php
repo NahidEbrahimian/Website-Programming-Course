53d76b4dbef530a1312f9095281fb0a2ee474bf7
Nahid Ebrahimian <nsebrahimian@gmail.com>
2022-05-01 09:09:35 +0430

    add files

---
+++ SocialNetwork/view/footer.php
@@ -0,0 +1,24 @@
+
+
+<footer  class="text-center mt-4 text-dark"  style="position: absolute; bottom: 80px; margin-top: 10px; width: 100%;">
+<hr>
+<p>
+    <?php echo date("Y"); ?>&copy;
+    Copyright
+</p>
+</footer>
+
+
+<!-- <script src="js/bootstrap.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script> -->
+<script type="text/javascript">
+    $('#date1').MdPersianDateTimePicker({
+      targetTextSelector: '#inputDate1',
+      dateFormat: 'yyyy-MM-dd',
+
+    });
+  </script>
+
+</body>
+
+</html>
