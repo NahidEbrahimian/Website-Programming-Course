cf7d2653461f208dc3d997934361a1a86e10d98d
Nahid Ebrahimian <nsebrahimian@gmail.com>
2022-05-16 16:20:38 +0430

    add controller files of view folder

--- SocialNetwork/controller/home.php
+++ SocialNetwork/controller/home.php
@@ -4,7 +4,7 @@
     // $posts = $db->query("SELECT * FROM posts");
     // $users = $db->query("SELECT * FROM users");
     $posts = $db->query("SELECT * FROM users  INNER JOIN posts ON posts.user_id = users.id");
-    $comments=$db->query("SELECT * FROM comments INNER JOIN users ON comments.user_id=users.id INNER JOIN posts ON comments.post_id=posts.id" );
+    $comments=$db->query("SELECT * FROM comments INNER JOIN users ON comments.user_id = users.id INNER JOIN posts ON comments.post_id = posts.id" );
 
     require "view/home.php";
 ?>
