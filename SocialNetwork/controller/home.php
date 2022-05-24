2d1e842fe3e0d208391ec09fd8beaba65ceb0cd3
Nahid Ebrahimian <nsebrahimian@gmail.com>
2022-05-24 21:47:20 +0430

    add comments and likes

--- SocialNetwork/controller/home.php
+++ SocialNetwork/controller/home.php
@@ -1,10 +1,37 @@
 <?php
     include "model/database.php";
+    include "controller/functions.php";
+    
+    $user_id = $_SESSION["user_id"];
+    $posts = $db->query("SELECT *, users.id AS user_id, 
+                        posts.id AS post_id
+                        FROM posts INNER JOIN users 
+                        ON posts.user_id = users.id ORDER BY time DESC");
 
-    // $posts = $db->query("SELECT * FROM posts");
-    // $users = $db->query("SELECT * FROM users");
-    $posts = $db->query("SELECT * FROM users  INNER JOIN posts ON posts.user_id = users.id");
-    $comments=$db->query("SELECT * FROM comments INNER JOIN users ON comments.user_id = users.id INNER JOIN posts ON comments.post_id = posts.id" );
+    // $following = $db->query("SELECT * FROM follows WHERE followers_id = $user_id");
+    // $user_id = $_SESSION["user_id"];
+    // $posts = $db->query("SELECT *, users.id AS user_id, 
+    // posts.id AS post_id
+    // FROM posts
+    // INNER JOIN follows 
+    // ON follows.followers_id = posts.user_id
+    // INNER JOIN users ON posts.user_id = follows.followers_id
+    // -- WHERE posts.user_id = $user_id
+    // ORDER BY time DESC");
 
+
+    $posts_array = array();
+    
+    foreach($posts as $post)
+    {
+        $post_id = $post["post_id"];
+        $post["comments"] = $db->query("SELECT * FROM comments 
+                                        INNER JOIN users 
+                                        ON comments.user_id = users.id
+                                        WHERE post_id = $post_id");
+        $post["likes"] = $db->query("SELECT COUNT(*) AS count FROM likes WHERE post_id = $post_id")->fetch_assoc();
+        $posts_array[] = $post; // append in python
+    }
+    
     require "view/home.php";
 ?>
--- SocialNetwork/controller/personal_profile.php
+++ SocialNetwork/controller/personal_profile.php
@@ -8,5 +8,7 @@ $posts = $db->query("SELECT * FROM posts WHERE user_id = $user_id");
 $posts_count = $posts->num_rows;
 $user = $db->query("SELECT * FROM users WHERE id = $user_id")->fetch_assoc();
 
+$comments = $db->query("SELECT * FROM comments INNER JOIN users ON comments.user_id=users.id INNER JOIN posts ON comments.post_id=posts.id" );
+
 require "view/personal_profile.php";
 ?>
