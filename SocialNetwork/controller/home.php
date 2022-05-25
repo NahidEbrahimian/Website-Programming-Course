9fd8f8eb0eccef45cb9481349608a42ce968f1cc
Nahid Ebrahimian <nsebrahimian@gmail.com>
2022-05-25 10:49:35 +0430

    reading all data from database

--- SocialNetwork/controller/home.php
+++ SocialNetwork/controller/home.php
@@ -6,18 +6,9 @@
     $posts = $db->query("SELECT *, users.id AS user_id, 
                         posts.id AS post_id
                         FROM posts INNER JOIN users 
-                        ON posts.user_id = users.id ORDER BY time DESC");
-
-    // $following = $db->query("SELECT * FROM follows WHERE followers_id = $user_id");
-    // $user_id = $_SESSION["user_id"];
-    // $posts = $db->query("SELECT *, users.id AS user_id, 
-    // posts.id AS post_id
-    // FROM posts
-    // INNER JOIN follows 
-    // ON follows.followers_id = posts.user_id
-    // INNER JOIN users ON posts.user_id = follows.followers_id
-    // -- WHERE posts.user_id = $user_id
-    // ORDER BY time DESC");
+                        ON posts.user_id = users.id
+                        WHERE posts.user_id IN (SELECT followings_id FROM follows WHERE followers_id = $user_id)
+                        ORDER BY time DESC");
 
 
     $posts_array = array();
@@ -29,6 +20,12 @@
                                         INNER JOIN users 
                                         ON comments.user_id = users.id
                                         WHERE post_id = $post_id");
+                                        
+        $post["num_comments"] = $db->query("SELECT COUNT(*) AS count FROM comments 
+                                            INNER JOIN users 
+                                            ON comments.user_id = users.id
+                                            WHERE post_id = $post_id")->fetch_assoc();;
+                                            
         $post["likes"] = $db->query("SELECT COUNT(*) AS count FROM likes WHERE post_id = $post_id")->fetch_assoc();
         $posts_array[] = $post; // append in python
     }
--- SocialNetwork/controller/personal_profile.php
+++ SocialNetwork/controller/personal_profile.php
@@ -1,5 +1,6 @@
 <?php
 include "model/database.php";
+include "controller/functions.php";
 
 $username = $_SESSION["user_name"];
 $user_id = $_SESSION["user_id"];
@@ -8,7 +9,29 @@ $posts = $db->query("SELECT * FROM posts WHERE user_id = $user_id");
 $posts_count = $posts->num_rows;
 $user = $db->query("SELECT * FROM users WHERE id = $user_id")->fetch_assoc();
 
-$comments = $db->query("SELECT * FROM comments INNER JOIN users ON comments.user_id=users.id INNER JOIN posts ON comments.post_id=posts.id" );
 
+$posts_array = array();
+
+foreach($posts as $post)
+{
+    $post_id = $post["id"];
+    $post["comments"] = $db->query("SELECT * FROM comments 
+                                    INNER JOIN users 
+                                    ON comments.user_id = users.id
+                                    WHERE post_id = $post_id");
+                                    
+    $post["num_comments"] = $db->query("SELECT COUNT(*) AS count FROM comments 
+                                        INNER JOIN users 
+                                        ON comments.user_id = users.id
+                                        WHERE post_id = $post_id")->fetch_assoc();
+    
+    $post["likes"] = $db->query("SELECT COUNT(*) AS count FROM likes WHERE post_id = $post_id")->fetch_assoc();
+    $post["followers"] = $db->query("SELECT COUNT(*) AS count FROM follows WHERE followings_id = $user_id")->fetch_assoc();
+    $post["followings"] = $db->query("SELECT COUNT(*) AS count FROM follows WHERE followers_id = $user_id")->fetch_assoc();
+    
+    $posts_array[] = $post; // append in python
+}
+
+    
 require "view/personal_profile.php";
 ?>
