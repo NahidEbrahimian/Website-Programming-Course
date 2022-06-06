1d9791d25a17ab33849d7bed0bc145dcf26af980
Nahid Ebrahimian <nsebrahimian@gmail.com>
2022-06-06 21:10:59 +0430

    fixed

--- SocialNetwork/controller/follow_unfollow_user.php
+++ SocialNetwork/controller/follow_unfollow_user.php
@@ -2,21 +2,18 @@
 
 include "model/database.php";
 $user_follow = $_POST["user-follow"];
-// $post_id = $_POST["post_id"];
 $user_id = $_SESSION["user_id"];
 
 $count = $db->query("SELECT * FROM follows WHERE followings_id = $user_follow AND followers_id = $user_id")->num_rows;
-
 if($count == 0)
 {
     $db->query("INSERT INTO follows(followings_id, followers_id) VALUES($user_follow, $user_id)");
-    echo "0";
+    echo "1";
 }
 else
 {
     $db->query("DELETE FROM follows WHERE followings_id = $user_follow AND followers_id = $user_id");
-    echo "1";
+    echo "0";
 }
 
-// header("Location: home.php");
 ?>
