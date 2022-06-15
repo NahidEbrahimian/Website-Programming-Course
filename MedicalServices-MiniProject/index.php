5ae5b594cda857ce1937c6d38b0ff4e72e46165a
Nahid Ebrahimian <nsebrahimian@gmail.com>
2022-06-15 12:06:47 +0430

    add routing file

---
+++ MedicalServices-MiniProject/index.php
@@ -0,0 +1,75 @@
+<?php
+
+    session_start();
+    $request = $_SERVER["REQUEST_URI"];
+    $request = str_replace("/MedicalServices-MiniProject", "", $request);
+    date_default_timezone_set("Asia/Tehran");
+
+    # GET USER LOGIN ID FOR WOLLOWING
+    if(isset($_GET["user-register"]))
+    {
+        $user_register = $_GET["user-register"];
+        if($user_register == 1)
+        {
+            $_SESSION["user-register"] = "patient";
+        }
+        else
+        {
+            $_SESSION["user-register"] = "doctor";
+        }
+    }
+    else
+    {
+        $user_register = "";
+        $_SESSION["user-register"] = "";
+    }
+
+    switch ($request)
+    {
+        case("/"):
+            require "controller/index.php";
+            break;
+
+        case ("/registration_form.php?user-register=$user_register"):
+            require "controller/registration_form.php";
+            break;
+
+        case ("/registration_process.php?user-register=$user_register"):
+            require "controller/registration_process.php";
+            break;
+
+        case ("/user_page.php"):
+            require "controller/user_page.php";
+            break;
+
+        case ("/login"):
+            require "controller/login.php";
+            break;
+
+        case ("/search"):
+            require "controller/search.php";
+            break;
+
+
+        case("/index.php"):
+            require "controller/index.php";
+            break;
+            
+        case("/login.php"):
+            require "controller/index.php";
+            break;
+            
+        case("/logout"):
+            require "controller/logout.php";
+            break;
+
+        case("/add_post_proccess.php"):
+            require "controller/add_post_proccess.php";
+            break;  
+                  
+        default:
+            require "controller/404.php";
+            break;
+    }
+
+?>
