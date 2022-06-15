6d789625bce0aaa121cf16f894d613627c78d58b
Nahid Ebrahimian <nsebrahimian@gmail.com>
2022-06-15 12:02:53 +0430

    add database

---
+++ MedicalServices-MiniProject/model/database.php
@@ -0,0 +1,17 @@
+<?php
+    // creat mysql object
+    $db = new mysqli("localhost", "root", "", "medical_services");
+    // $db = new mysqli("localhost", "id18865666_socialnetworkdb", "7YY0T)JO<Z3O4yY-", "id18865666_socialnetwork");
+
+
+    if($db->connect_error)
+    {
+        echo "error";
+        echo $db->connect_error;
+    }
+
+    else
+    {
+        $db->query("SET CHARACTER SET utf8");
+    }
+?>
